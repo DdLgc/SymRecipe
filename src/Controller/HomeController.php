9a54cbb8fa47cfedece61e2becb853702c8f1112
David Lgc <legouellecd@gmail.com>
2023-01-20 02:08:04 +0100

    add home page + _header

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name:'home.index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return  $this->render('home.html.twig');
+    }
+}
