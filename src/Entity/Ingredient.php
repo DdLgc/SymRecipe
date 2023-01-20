0f17243850265c3d4764fd9cafc211b0c6a1f2e1
David Lgc <legouellecd@gmail.com>
2023-01-20 03:02:53 +0100

    add #ASSERT contraints Ingredients

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use App\Repository\IngredientRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+
+use Symfony\Component\Validator\Constraints as Assert;
+
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
 class Ingredient
 {
@@ -13,13 +16,20 @@ class Ingredient
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(type:'string', length: 50)]
+    #[Assert\NotBlank()]
+    #[Assert\Lenght(min:2, max:50)]
     private ?string $name = null;
 
+
     #[ORM\Column]
+    #[Assert\NotNull()]
+    #[Assert\Positive()]
+    #[Assert\LessThan(200)]
     private ?float $price = null;
 
     #[ORM\Column]
+    #[Assert\NotNull()]
     private ?\DateTimeImmutable $createdAt = null;
 
     public function getId(): ?int
