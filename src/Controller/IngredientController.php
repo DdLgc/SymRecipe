6c67d570288ee145d26403221bb049ee3a8469c3
David Lgc <legouellecd@gmail.com>
2023-01-24 11:38:22 +0100

    add Update ingredient of CRUD

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -16,7 +16,7 @@ class IngredientController extends AbstractController
 {
     /** 
      * /**
-      * function display all ingredients
+      * controller function display all ingredients
       *
       * @param IngredientRepository $repository
       * @param PaginatorInterface $paginator
@@ -39,8 +39,14 @@ class IngredientController extends AbstractController
                 'ingredients' => $ingredients
             ]);
     }
+    /**
+     *  controller function show a form which create ingredient
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
     #[Route('/ingredient/nouveau', 'ingredient.new', methods: ['GET', 'POST'])]
-
     public function new(
         Request $request,
         EntityManagerInterface $manager
@@ -60,9 +66,38 @@ class IngredientController extends AbstractController
                 'success',
                 'Votre ingredient a été ajouté avec succès !'
             );
+            return $this->redirectToRoute('ingredient.index');     
         } 
-            // $this->redirectToRoute('ingredient.index');      Fonctionne pas
         return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    /**
+     * Funcion edit ingredient
+     * 
+     * @param Ingredient $ingredient
+     * @param Request $request
+     * **/
+    #[Route('/ingredient/edition/{id}', 'ingredient.edit', methods: ['GET', 'POST'])]
+        public function edit(Ingredient $ingredient,
+        Request $request, 
+        EntityManagerInterface $manager 
+        ) : Response    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient= $form->getData();
+        
+            $manager->persist($ingredient);
+            $manager->flush();
+            
+            $this->addFlash(
+                'success',
+                'Votre ingredient a été modifié avec succès !'
+            );
+            return $this->redirectToRoute('ingredient.index');     
+        } 
+        return  $this->render('pages/ingredient/edit.html.twig' ,[
             'form' => $form->createView()
         ]);
     }
